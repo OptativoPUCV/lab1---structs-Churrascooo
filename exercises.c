699597440ed8b932e4080d8867a4e7c820758b6f
Your Name <you@example.com>
2024-03-16 00:18:07 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -196,20 +196,20 @@ Nodo *crearListaEnlazada(int arr[], int size)
   Nodo *cabeza = NULL;
   Nodo *actual = NULL;
   for (int i = 0; i < size; i++)
-  {
-    Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
-    nuevoNodo->numero = arr[i];
-    nuevoNodo->siguiente = NULL;
-
-    if (cabeza == NULL)
     {
-      cabeza = nuevoNodo;
+      Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
+      nuevoNodo->numero = arr[i];
+      nuevoNodo->siguiente = NULL;
+      if (cabeza == NULL)
+      {
+        cabeza = nuevoNodo;
+        actual = nuevoNodo;
+      }
+      else
+      {
+        actual->siguiente = nuevoNodo;
+        actual = nuevoNodo;
+      }
     }
-    else
-    {
-      actual->siguiente = nuevoNodo;
-      actual = nuevoNodo;
-    }
-  }
   return cabeza;
 }
